30f79088cd4446bb47903a6474ed7bdb2a606f87
Bing Ran <bing.ran@gmail.com>
2012-11-03 23:00:11 +0800

    changed reference to Japid42 on github repository

--- project/Build.scala
+++ project/Build.scala
@@ -9,13 +9,13 @@ object ApplicationBuild extends Build {
 
     val appDependencies = Seq(
       "org.hibernate" % "hibernate-entitymanager" % "3.6.9.Final"
-      ,"japid42" % "japid42_2.9.1" % "0.5.2"
+      ,"japid42" % "japid42_2.9.1" % "0.5.3"
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = JAVA).settings(
       ebeanEnabled := false,
-      resolvers += "Local Play Repository" at "/Users/bran/projects/playscala/repository/local"
-
+//      resolvers += "Local Play Repository" at "/Users/bran/projects/playscala/repository/local"
+      resolvers += Resolver.url("My GitHub Play Repository", url("http://branaway.github.com/releases/"))(Resolver.ivyStylePatterns)
     )
 
 }
