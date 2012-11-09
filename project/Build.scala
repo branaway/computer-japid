8944a36e1a5963042722b246a103c0a067facac9
Bing Ran <bing.ran@gmail.com>
2012-11-09 23:16:10 +0800

    updated to use 0.5.5

--- project/Build.scala
+++ project/Build.scala
@@ -9,15 +9,15 @@ object ApplicationBuild extends Build {
 
     val appDependencies = Seq(
       "org.hibernate" % "hibernate-entitymanager" % "3.6.9.Final"
-      ,"japid42" % "japid42_2.9.1" % "0.5.4"
+      ,"japid42" % "japid42_2.9.1" % "0.5.5"
     )
 
     val foo = PlayProject("foo", "0.1", appDependencies, path = file("modules/foo"))
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = JAVA).settings(
       ebeanEnabled := false,
-      resolvers += "Local Play Repository" at "/Users/bran/projects/playscala/repository/local"
-//      resolvers += Resolver.url("Japid on Github", url("http://branaway.github.com/releases/"))(Resolver.ivyStylePatterns)
+//      resolvers += "Local Play Repository" at "/Users/bran/projects/playscala/repository/local"
+      resolvers += Resolver.url("Japid on Github", url("http://branaway.github.com/releases/"))(Resolver.ivyStylePatterns)
     ).dependsOn(foo)
 
 }
