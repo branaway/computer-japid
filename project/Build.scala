6d7fa9e523570735d51433f945ab61163937759d
Bing Ran <bing.ran@gmail.com>
2012-11-13 08:07:16 +0800

    added missing depndency in the foo sub project

--- project/Build.scala
+++ project/Build.scala
@@ -12,7 +12,9 @@ object ApplicationBuild extends Build {
       ,"japid42" % "japid42_2.9.1" % "0.6"
     )
 
-    val foo = PlayProject("foo", "0.1", appDependencies, path = file("modules/foo"))
+    val foo = PlayProject("foo", "0.1", appDependencies, path = file("modules/foo")).settings(
+      resolvers += Resolver.url("Japid on Github", url("http://branaway.github.com/releases/"))(Resolver.ivyStylePatterns)
+    )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = JAVA).settings(
       ebeanEnabled := false,
