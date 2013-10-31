48f16a6a9f05743b4a86916f9da45a334b81c44b
Bing Ran <bing.ran@gmail.com>
2013-10-31 10:00:25 +0800

    updated to use github.io as the repository root

--- project/Build.scala
+++ project/Build.scala
@@ -13,7 +13,7 @@ object ApplicationBuild extends Build {
     		javaJpa,
     		cache,
     		"org.hibernate" % "hibernate-entitymanager" % "3.6.9.Final",
-    		"japid42" % "japid42_2.10" % "0.9.12"
+    		"japid42" % "japid42_2.10" % "0.9.13"
     )
 
 //    val foo = play.Project("foo", "0.1", appDependencies, path = file("modules/foo")).settings(
@@ -21,7 +21,7 @@ object ApplicationBuild extends Build {
 //    )
 
     val main = play.Project(appName, appVersion, appDependencies).settings(
-    		resolvers += Resolver.url("Japid on Github", url("http://branaway.github.com/releases/"))(Resolver.ivyStylePatterns)
+    		resolvers += Resolver.url("Japid on Github", url("http://branaway.github.io/releases/"))(Resolver.ivyStylePatterns)
     		,ebeanEnabled := false
 //    		,unmanagedResourceDirectories in Compile  <+= baseDirectory( _ / "japidroot" )
 
