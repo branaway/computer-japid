7dfb89ed00f8679ec58dbef0b547d08500e6b51a
Bing Ran <bing.ran@gmail.com>
2013-11-14 19:54:32 +0800

    using japid 0.9.14 which uses play 2.2.1

--- project/Build.scala
+++ project/Build.scala
@@ -5,26 +5,18 @@ import play.Project._
 object ApplicationBuild extends Build {
 
     val appName         = "computer-japid"
-    val appVersion      = "1.1"
+    val appVersion      = "1.2"
 
     val appDependencies = Seq(
-  //  		javaCore,
-  //  		javaJdbc,
     		javaJpa,
+    		cache,
     		"org.hibernate" % "hibernate-entitymanager" % "3.6.9.Final",
     		"japid42" % "japid42_2.10" % "0.9.14"
     )
 
-    val foo = play.Project("foo", "0.1", appDependencies, path = file("modules/foo")).settings(
-    		resolvers += Resolver.url("Japid on Github", url("http://branaway.github.com/releases/"))(Resolver.ivyStylePatterns)
-    )
-
     val main = play.Project(appName, appVersion, appDependencies).settings(
-    		ebeanEnabled := false
-//      	,resolvers += "Local Play Repository" at "/Users/bran/projects/playscala/repository/local" 
-//    		,resolvers += Resolver.url("Japid on Github", url("http://branaway.github.com/releases/xxx"))(Resolver.ivyStylePatterns)
-//    		, resourceDirectories in (Compile, run) <+= baseDirectory / "japidroot"
-    ).dependsOn(foo)
-
+    		resolvers += Resolver.url("Japid on Github", url("http://branaway.github.io/releases/"))(Resolver.ivyStylePatterns)
+    		,ebeanEnabled := false
+    )
 }
             
