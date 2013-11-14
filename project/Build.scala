56187d646328217563054d5ad601893704a1868a
Bing Ran <bing.ran@gmail.com>
2013-11-14 22:59:27 +0800

    added japidroot in jar distribution in the build.scala

--- project/Build.scala
+++ project/Build.scala
@@ -11,12 +11,14 @@ object ApplicationBuild extends Build {
     		javaJpa,
     		cache,
     		"org.hibernate" % "hibernate-entitymanager" % "3.6.9.Final",
-    		"japid42" % "japid42_2.10" % "0.9.14"
+    		"japid42" % "japid42_2.10" % "0.9.14.1"
     )
 
     val main = play.Project(appName, appVersion, appDependencies).settings(
     		resolvers += Resolver.url("Japid on Github", url("http://branaway.github.io/releases/"))(Resolver.ivyStylePatterns)
     		,ebeanEnabled := false
+    		// comment out the following line in dev mode so changes in Japid scripts won't trigger app reloading
+    		,unmanagedResourceDirectories in Compile  <+= baseDirectory( _ / "japidroot" )
     )
 }
             
